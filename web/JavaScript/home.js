16fa3a70ac55200958cf544cf3fee4c8cc4212dd
CodePorter <101229747+lzb200244@users.noreply.github.com>
2022-04-10 20:30:30 +0800

    Add files via upload

---
+++ web/JavaScript/home.js
@@ -0,0 +1,11 @@
+function about_open() {
+    document.getElementById('about_our').style.display = 'block';
+
+}
+function about_close() {
+    document.getElementById('about_our').style.display = 'none';
+}
+function join_us(){
+    alert("敬请期待...")
+
+}
